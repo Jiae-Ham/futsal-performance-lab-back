421d1271b857f6ba9df3d34b2f65b4e06f993a99
dhgus1231 <74444490+dhgus1231@users.noreply.github.com>
2025-05-21 14:31:32 +0900

    init

---
+++ back/src/main/java/com/alpaca/futsal_performance_lab_back/FutsalPerformanceLabBackApplication.java
@@ -0,0 +1,13 @@
+package com.alpaca.futsal_performance_lab_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutsalPerformanceLabBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutsalPerformanceLabBackApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/alpaca/futsal_performance_lab_back/FutsalPerformanceLabBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpaca.futsal_performance_lab_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutsalPerformanceLabBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
